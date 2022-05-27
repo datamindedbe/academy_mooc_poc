eb8b990027e33fb3e6aac596b385f43a37a153b7
nathan.derave <nathan.derave@dataminded.be>
2022-05-27 13:41:30 +0200

    Adapted the docker compose stack and created a smoke_test dag

--- references/airflow/pre-setup.sh
+++ references/airflow/pre-setup.sh
@@ -8,6 +8,7 @@ sudo usermod -a -G docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/download/v2.5.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Create the Airflow stack prerequisites folders.
+# Create the Airflow stack prerequisites folders and upload the smoke test dag.
 mkdir -p ./dags ./logs ./plugins
 echo -e "AIRFLOW_UID=$(id -u)" > .env
+sudo curl -L https://raw.githubusercontent.com/datamindedbe/academy_mooc_poc/main/references/airflow/smoke_test_dag.py -o ./dags/smoke_test_dag.py
